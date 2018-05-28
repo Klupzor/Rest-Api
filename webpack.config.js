996018cb2721277aff7fb36282aa51d54facc7ea
klupzor <lopezgand@gmail.com>
2018-05-27 22:22:39 -0500

    configurando webpack para el back-end

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+module.exports = {
+  target: "node",
+  mode: "development",
+  entry: {
+    api: ["./src/index.js"]
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+},
+  externals: [nodeExternals()],
+};
